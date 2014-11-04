213dd277eb8b783c1df248e200772dabf85e81bf
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-11-03 17:10:47 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var fs = require('fs');
+var todos = require('./todos.json');
+var jade = require('jade');
+var mustache = require('mustache');
+var dust = require('dustjs-linkedin');
+var ejs = require('ejs');
+var nunjucks = require('nunjucks');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/jade', function(req, res) {
+  var options = {todos: todos};
+  res.send(jade.renderFile('./jade/index.jade', options));
+})
+
+app.get('/mustache', function(req, res) {
+  var template = fs.readFileSync('./mustache/index.mustache', 'utf8');
+  var partials = {
+    item: fs.readFileSync('./mustache/item_partial.mustache', 'utf8')
+  };
+  var locals = {todos: todos};
+  res.send(mustache.render(template, locals, partials));
+});
+
+app.get('/dust', function(req, res) {
+  function register(name) {
+    var template = dust.compile(fs.readFileSync('./dust/' + name + '.dust', 'utf8'), name);
+    dust.loadSource(template);
+  }
+
+  register('index');
+  register('layout');
+  register('item_partial');
+
+  var delay = 1;
+
+  var locals = {
+    todos: todos,
+    stream: function(chunk, context, bodies) {
+      return chunk.map(function(chunk) {
+        setTimeout(function() {
+          chunk
+            .render(bodies.block, context)
+            .end();
+        }, delay++ * 1000);
+      });
+    }
+  };
+
+  dust
+    .stream('index', locals)
+    .on('error', function(err) {
+      console.log(err.message);
+      res.send(err.message);
+    })
+    .pipe(res)
+    ;
+});
+
+app.get('/nunjucks', function(req, res) {
+  var locals = {todos: todos};
+  nunjucks.render('./nunjucks/index.html', locals, function(err, result) {
+    if(err) {
+      return res.send(err.message);
+    }
+    res.send(result);
+  });
+});
+
+app.get('/ejs', function(req, res) {
+  var template = fs.readFileSync('./ejs/index.ejs', 'utf8');
+  var options = {
+    filename: './ejs/index.ejs',
+    todos: todos
+  };
+  res.send(ejs.render(template, options));
+});
+
+app.get('/eco', function(req, res) {
+
+});
+
+app.get('/haml', function(req, res) {
+
+});
+
+app.listen(3000);
+console.log('Example is listening on http://127.0.0.1:3000');
